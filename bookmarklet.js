41d200df0c07b767e5c728909aecbc5c7fafd76a
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 18:41:24 -0500

    removed window.alert from bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,6 @@ j.src = "http://code.jquery.com/jquery-3.6.0.min.js";
 document.head.appendChild(j);
 var m = setTimeout(function() {
         $.getScript("http://localhost:8080/" + "reader.js", function() {
-            window.alert("Loading RSM...");
             main();
         });
     },1000);    

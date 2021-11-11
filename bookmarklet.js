ec326b0a8d753e260b692f6f3c422af6da56ee8f
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 17:27:09 -0500

    slightly shortened bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,7 @@
-var jQueryLib = document.createElement("script");
-jQueryLib.src = "http://code.jquery.com/jquery-3.6.0.min.js";
-document.head.appendChild(jQueryLib);
-var myCode = setTimeout(function() {
+var j = document.createElement("script");
+j.src = "http://code.jquery.com/jquery-3.6.0.min.js";
+document.head.appendChild(j);
+var m = setTimeout(function() {
         $.getScript("http://localhost:8080/" + "reader.js", function() {
         main();
         });

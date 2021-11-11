4077440bb2aca2452d261b9d14d18d55426505a7
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 15:45:31 -0500

    write to about:blank

--- reader.js
+++ reader.js
@@ -1,4 +1,13 @@
 function main() {
-    window.alert('Worked');
+
+    const newStyle = document.createElement("link");
+    newStyle.rel = "stylesheet";
+    newStyle.href = "http://localhost:8080/styles.css";
+    document.head.appendChild(newStyle);
+
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("Hello World!");
+    newDiv.appendChild(newContent);
+    newDiv.id = "title";
+    document.body.appendChild(newDiv);
   }
-  
